1522c8c516d2b7c6e9bb1b39fa9d15a6725dcd65
Shak <shak.developer@gmail.com>
2019-10-10 22:25:38 +0100

    Pushing product component

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,20 +1,87 @@
 <template>
   <v-container>
     <v-layout wrap>
-      <v-flex mb-4>
+        <v-flex>
         <h1 class="display-2 font-weight-bold mb-3">
-          Day 1/30
+          Day {{days}}/30
         </h1>
       </v-flex>
     </v-layout>
+
+    <v-layout wrap>
+     <!-- Dashboard? -->
+     <v-flex xs12>
+       <v-layout row>
+         <v-flex xs12 mb-4 v-for="(product,i) in products" :key="i">
+          <product :product="product">
+          </product>
+         </v-flex>
+
+       </v-layout>
+     </v-flex>
+    </v-layout>
+
   </v-container>
 </template>
 
 <script>
-export default {
-  data: () => ({
+import Product from "./Product";
 
+export default {
+  components: {
+    Product
+  },
+  data: () => ({
+    days:1,
+    products:[
+      {
+        id: 1,
+        name:"Cocaine",
+        price:0, 
+        pricerange:"high",
+
+      },
+      {
+        id:2,
+        name:"Heroin", 
+        price:0, 
+        pricerange:"high", 
+      },
+      {
+        id:3,
+        name:"Speed", 
+        price:0, 
+        pricerange:"high", 
+      },
+      {
+        id:4,
+        name:"Weed",
+        price:0,  
+        pricerange:"high", 
+      },
+    ],
+    bank: 2000,
+    inventoryAmount: 100,
+    inventoryProducts:[
+      //Push object of products into here that includes name, id, and price bought at
+    ],
+    locations:[
+      {name:"London"},
+      {name:"Manchester"},
+      {name:"Birmingham"},
+      {name:"Leeds"},
+      {name:"Bristol"},
+    ],
+    debt: 2000,
+    events:[
+      {
+
+      }
+    ]
   }),
+
+
+
 };
 </script>
 
---
+++ src/components/Product.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-card>
+        <v-card-title>{{product.name}}</v-card-title>
+        <v-card-text>£{{product.price}}</v-card-text>
+        <v-layout>
+            <v-flex xs6>
+                <v-btn>Buy</v-btn>
+                <v-btn>Sell</v-btn>
+            </v-flex>
+        </v-layout>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props:{
+        product: Object
+
+    },
+    data:()=>({
+
+    })
+
+}
+</script>
+
+<style>
+
+</style>
